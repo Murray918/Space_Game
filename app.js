e96579d5226578f13e86bddbd85aaa0d2087c21c
Sithiz <Joshklaas1@gmail.com>
2018-12-30 21:25:31 -0600

    got some photos and the alien fleet arrived

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min; 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
@@ -10,59 +9,50 @@ function random(min, max) {
 
 
 let earthShip = {
-	hull: 20,
-	firePower: 5,
-	accuracy: function() {
-		let hit = Math.random()
-		if(hit <= .7){
-			console.log('hit')
-		}else{
-			console.log('miss')
-		}
-	}
+    hull: 20,
+    firePower: 5,
+    accuracy: function() {
+        let hit = Math.random()
+        if (hit <= .7) {
+            console.log('hit')
+        } else {
+            console.log('miss')
+        }
+    }
 
 }
 
 
-let alienAccuracy = function(){
-	let int = random(.6,.8)
-	let hit = Math.random()
-	if(hit <= int){
-		return 'hit'
-	}else{
-		return 'miss'
-	}
+let alienAccuracy = function() {
+    let int = random(.6, .8)
+    let hit = Math.random()
+    if (hit <= int) {
+        return 'hit'
+    } else {
+        return 'miss'
+    }
 
 
 }
 
 class alienShip {
-	constructor(){
-		this.hull = random(3,6)
-		this.firePower = random(2,4)
-		this.accuracy = alienAccuracy()
+    constructor() {
+        this.hull = random(3, 6)
+        this.firePower = random(2, 4)
+        this.accuracy = alienAccuracy
 
-	}
+    }
 
 }
 
 
 
-let alienShip1 = new alienShip()
-
-
-console.log(alienShip1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let alienFleet = []
+let theAliensCome = function() {
+    alienFleet.push(new alienShip)
+    alienFleet.push(new alienShip)
+    alienFleet.push(new alienShip)
+    alienFleet.push(new alienShip)
+    alienFleet.push(new alienShip)
+    alienFleet.push(new alienShip)
+}
