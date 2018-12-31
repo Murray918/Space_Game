3df27f699895b753fcf622cc4635f9362b413e3f
Sithiz <Joshklaas1@gmail.com>
2018-12-31 01:56:20 -0600

    slight update

--- app.js
+++ app.js
@@ -59,14 +59,13 @@ let theAliensCome = function() {
 
 
 let earthAttack = prompt('Do you wish to do battle? \n if so type yes')
-
-if(earthAttack.toLowerCase() === 'yes'){
-	theAliensCome()
-	console.log('Earth is under attack its your move')
-} else if(earthAttack != 'yes'){
-	console.log('the earth is destroyed')
+if (earthAttack.toLowerCase() === 'yes') {
+    theAliensCome()
+    console.log('Earth is under attack its your move, \n There are 6 alien ships you have the first attack')
+} else if (earthAttack != 'yes') {
+    console.log('the earth is destroyed')
     $('html').hide()
-}
+} 
 
 
 
@@ -74,38 +73,38 @@ if(earthAttack.toLowerCase() === 'yes'){
 
 
 
-let attackPhaseHuman = function(){
- if(alienFleet.length != 0){
-    if(earthShip.accuracy() === 'hit'){
-        alienFleet[0].hull -= earthShip.firePower
-        console.log('you hit')
-        if(alienFleet[0].hull <=0){ //if you kill the ship you get to attack first again
-            console.log('defeated alien')
-            alienFleet.shift()
-            console.log(alienFleet.length + ' remaining ships')
-            let nextRound = prompt('Do you wish to continue?')//currently to show win screen you need to attack when there is no more alien ships left
-            if(nextRound.toLowerCase() != 'yes'){
-                gameOver()
+let attackPhaseHuman = function() {
+    if (alienFleet.length != 0) {
+        if (earthShip.accuracy() === 'hit') {
+            alienFleet[0].hull -= earthShip.firePower
+            console.log('you hit')
+            if (alienFleet[0].hull <= 0) { //if you kill the ship you get to attack first again
+                console.log('defeated alien')
+                alienFleet.shift()
+                console.log(alienFleet.length + ' remaining ships')
+                let nextRound = prompt('Do you wish to continue?') //currently to show win screen you need to attack when there is no more alien ships left
+                if (nextRound.toLowerCase() != 'yes') {
+                    gameOver()
+                }
+            } else { //aliens attack at the end of each phase
+
+                attackPhaseAlien()
             }
-        } else{ //aliens attack at the end of each phase
-            
+
+        } else { //aliens attack at the end of each phase
+            console.log('miss')
             attackPhaseAlien()
         }
-
-    } else{ //aliens attack at the end of each phase
-        console.log('miss')
-        attackPhaseAlien()
+    } else {
+        gameOver()
     }
-}else {
-    gameOver()
-}
 }
 
-let attackPhaseAlien = function(){
-    if(alienFleet[0].accuracy() === 'hit'){
+let attackPhaseAlien = function() {
+    if (alienFleet[0].accuracy() === 'hit') {
         earthShip.hull -= alienFleet[0].firePower
-        console.log("you've been hit, hull strength down to " + earthShip.hull )
-        if(earthShip.hull <=0){
+        console.log("you've been hit, hull strength down to " + earthShip.hull)
+        if (earthShip.hull <= 0) {
             gameOver()
         }
     } else {
@@ -113,13 +112,13 @@ let attackPhaseAlien = function(){
     }
 }
 
-let gameOver = function(){
-    if(alienFleet.length === 0){
-    $('body').hide()
-    $('html').append('<body><h1> YOU WIN</h1></body>')
-    } else{
-    $('body').hide()
-    $('html').append('<body><h1> YOU LOSE</h1></body>')
+let gameOver = function() {
+    if (alienFleet.length === 0) {
+        $('body').hide()
+        $('html').append('<body><h1> YOU WIN</h1></body>')
+    } else {
+        $('body').hide()
+        $('html').append('<body><h1> YOU LOSE</h1></body>')
     }
 }
 
@@ -130,9 +129,3 @@ $console.append("<button id='attack'>Attack</button> <button id='retreat'>Retrea
 
 $('#attack').click(attackPhaseHuman)
 $('#retreat').click(gameOver)
-
-
-
-
-
-
