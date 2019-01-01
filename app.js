02c286e9b399d184688551a5adbbc84b3e5f845b
Tristan Bennett <betriistan@gmail.com>
2018-12-31 20:07:27 -0500

    added name property to all objects

--- app.js
+++ app.js
@@ -16,16 +16,17 @@ class Ship {
     }
 }
 // create USS Assembly ship with pre-determined parameters
-let USSA = new Ship(20, 5, 0.7);
+let USSA = new Ship("USS Assembly", 20, 5, 0.7);
 
 // create array to hold 6 alien ships with randomly generated parameter values that fit within pre-determined range
 let alienFleet = [];
 
 // create for loop to create 6 alien ship objects and push them into the
 for (let i = 0; i < 6; i++) {
-    alienFleet[i] = new Ship((Math.floor(Math.random() * (7 - 3) + 3)), (Math.floor(Math.random() * (5 - 2) + 2)), (Math.random() * (0.8 - 0.6) + 0.6))
+    alienFleet[i] = new Ship(`Alien Ship ${i+1}`,(Math.floor(Math.random() * (7 - 3) + 3)), (Math.floor(Math.random() * (5 - 2) + 2)), (Math.random() * (0.8 - 0.6) + 0.6))
 }
 
+console.log(USSA);
 console.log(alienFleet[0])
 console.log(alienFleet[1])
 console.log(alienFleet[2])
