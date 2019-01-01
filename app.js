c94f24c53566cc0411dd1c749ef2a7ca27122714
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-31 20:51:16 -0500

    updated attack method in ship class, added pseudocode as comments for remaining tasks

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ class Ship {
     attack(enemy) {
         if (Math.random() > this.accuracy) {
             console.log(`${this.name} missed`);
+            return `${enemy.name} hull: ${enemy.hull}`
         } else {
             enemy.hull -= this.firepower;
-            console.log(`${enemy.name} took ${this.firepower} damage.`);
+            console.log(`${this.name} dealt ${this.firepower} damage.`);
+            return `${enemy.name} hull: ${enemy.hull}`
         }
     }
 }
@@ -26,11 +28,34 @@ for (let i = 0; i < 6; i++) {
     alienFleet[i] = new Ship(`Alien Ship ${i+1}`,(Math.floor(Math.random() * (7 - 3) + 3)), (Math.floor(Math.random() * (5 - 2) + 2)), (Math.random() * (0.8 - 0.6) + 0.6))
 }
 console.log(alienFleet[0]);
+console.log("==============================")
 console.log(USSA);
 console.log("==============================")
 console.log(alienFleet[0].attack(USSA));
-console.log(alienFleet[1].attack(USSA));
-console.log(alienFleet[2].attack(USSA));
+console.log("==============================")
+console.log(USSA.attack(alienFleet[0]));
+console.log("==============================")
+
+// loop for whole game: while USSA hull >0 and alienFleet.length > 0
+
+	// loop for one round: while USSA hull > 0 and alienFleet[i] hull > 0
+
+	// USSA.attack(alienFleet[i])
+
+	// if alienFleet[i].hull > 0 { alienFleet[i] attacks USSA }
+
+	// else { 
+		// remove 1st alien ship from alienFleet
+
+		// prompt player to attack next alien ship or retreat
+
+		// if retreat {
+		// console.log("Game Over")
+		// break;
+		// }
+
+	// }
+
 
 
 
